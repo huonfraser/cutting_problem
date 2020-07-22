82b087c4a493dcae5f73d248cb006c5076e02cbd
Huon Fraser <huon.fraser@gmail.com>
2020-07-23 10:23:40 +1200

    Added file loader, various small changes

--- skeleton.py
+++ skeleton.py
@@ -14,13 +14,30 @@ def load(file_name):
     Load data from a file , return a sequence of soln and width of the file
     :param file_name:
     :return: Loaded instanc
+
+    data format list of tuple [(id,width,height)]
     """
-    return Data(), 0
+    data = []
+    with open(file_name, "r") as file:
+        line1 = file.readline() #size line
+        line2 = file.readline() #area line
+        line3 = file.readline() #col names file
+        #print(line1)
+        #print(line2)
+        #print(line3)
+        area = line2.split(",")[1]
+        content_lines = file.readlines()
+        for line in content_lines:
+            line = line.replace("\n","")
+            splat = line.split(",",)
+            data.append((splat[0],splat[1],splat[2]))
+        print(data)
+    return Data(data, area), 0
 
 def search(data):
     """
     Search heuristic that takes a sequence of data and modifies them according to a neighbourhood
-    :param data:
+    :param data: data format list of tuple [(id,width,height)]
     :return:
     """
     return Data(data)
@@ -28,7 +45,7 @@ def search(data):
 def place(data):
     """
     Placement algorithm that takes a sequence of data and places them according to a heuristic
-    :param data:
+    :param data: data format list of tuple [(id,width,height)]
     :return:
     """
     return Solution()
@@ -76,17 +93,35 @@ class Data():
     """
     Holder class for input data set,
     represent a sequence of data
+
+    data format list of tuple [(id,width,height)]
     """
 
     data = []
+    area = 0
 
-    def __init__(self, data):
+    def __init__(self, data, area=None):
         self.data = data
+        if(area == None):
+            #calculate area
+            pass
+        else:
+            self.area = area
 
 
 class Solution():
+    """
+    Soln format list of tuple [(id,posx,posy,width,height)]
+    """
+
+    soln = []
+
+    def __init__(self,soln):
+        self.soln = soln
     """
     Holder class for soln representation
     """
 
 #design choice, - represent soln and data as seperate lists, vs single
+
+load("data\M1a.csv")
